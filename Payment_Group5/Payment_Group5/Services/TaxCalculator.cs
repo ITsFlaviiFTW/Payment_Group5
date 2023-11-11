00b03bad8e8bd26e14bd987fcae94966227e1e13
Nico Timmerman <timmerman.ny64@gmail.com>
2023-11-10 20:20:57 -0500

    Added calculate GST function

---
+++ Payment_Group5/Payment_Group5/Services/TaxCalculator.cs
@@ -0,0 +1,10 @@
+﻿namespace Payment_Group5.Services
+{
+    public class TaxCalculator
+    {
+        public static double calculateGST(double subTotal)
+        {
+            return subTotal * 0.13;
+        }
+    }
+}
